081c80b34e80c58d16b869080a2b85d41742d929
Alina <dvoryannikova.alina@gmail.com>
2021-11-10 20:27:36 +0200

    add react project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Header } from './components';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+
+import './scss/app.scss';
+
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route exact path="/" component={<App />} />
+        <Route exact path="/header" component={<Header />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
