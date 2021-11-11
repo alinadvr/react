efbec5efd0cb1f0f91c0d08a5ad5853e5874a870
Alina <dvoryannikova.alina@gmail.com>
2021-11-11 14:03:32 +0200

    add json

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import { Home, Cart } from './pages';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} exact />
+          <Route path="/" element={<Home items={pizzas} />} exact />
           <Route path="/cart" element={<Cart />} exact />
         </Routes>
       </div>
